80aa540699c005c9200a0817d8d41bd17af63948
Neil Johnson <nj.designs@protomail.com>
2025-05-22 17:26:16 +0100

    Change .clang-format

--- lib/include/core.h
+++ lib/include/core.h
@@ -55,31 +55,31 @@ static const uint32_t STK_SR_CNTIF = (1 << 0);
  *
  */
 struct PFICRegMap {
-  volatile uint32_t isr[4];      // 0xE000E000
-  volatile uint32_t rsvd1[4];    // 0xE000E010
-  volatile uint32_t ipr[4];      // 0xE000E020
-  volatile uint32_t rsvd2[4];    // 0xE000E030
-  volatile uint32_t ithresdr;    // 0xE000E040
-  volatile uint32_t rsvd3;       // 0xE000E044
-  volatile uint32_t cfgr;        // 0xE000E048
-  volatile uint32_t gisr;        // 0xE000E04C
-  volatile uint32_t vtfidr;      // 0xE000E050
-  volatile uint32_t rsvd4[3];    // 0xE000E054
-  volatile uint32_t vtfaddrr[4]; // 0xE000E060
-  volatile uint32_t rsvd5[36];   // 0xE000E070
-  volatile uint32_t ienr[4];     // 0xE000E100
-  volatile uint32_t rsvd6[28];   // 0xE000E110
-  volatile uint32_t irer[4];     // 0xE000E180
-  volatile uint32_t rsvd7[28];   // 0xE000E190
-  volatile uint32_t ipsr[4];     // 0xE000E200
-  volatile uint32_t rsvd8[28];   // 0xE000E210
-  volatile uint32_t iprr[4];     // 0xE000E280
-  volatile uint32_t rsvd9[28];   // 0xE000E290
-  volatile uint32_t iactr[4];    // 0xE000E300
-  volatile uint32_t rsvd10[60];  // 0xE000E310
-  volatile uint32_t iprior[64];  // 0xE000E400
-  volatile uint32_t rsvd11[516]; // 0xE000E500
-  volatile uint32_t sctlr;       // 0xE000ED10
+  volatile uint32_t isr[4];       // 0xE000E000
+  volatile uint32_t rsvd1[4];     // 0xE000E010
+  volatile uint32_t ipr[4];       // 0xE000E020
+  volatile uint32_t rsvd2[4];     // 0xE000E030
+  volatile uint32_t ithresdr;     // 0xE000E040
+  volatile uint32_t rsvd3;        // 0xE000E044
+  volatile uint32_t cfgr;         // 0xE000E048
+  volatile uint32_t gisr;         // 0xE000E04C
+  volatile uint32_t vtfidr;       // 0xE000E050
+  volatile uint32_t rsvd4[3];     // 0xE000E054
+  volatile uint32_t vtfaddrr[4];  // 0xE000E060
+  volatile uint32_t rsvd5[36];    // 0xE000E070
+  volatile uint32_t ienr[4];      // 0xE000E100
+  volatile uint32_t rsvd6[28];    // 0xE000E110
+  volatile uint32_t irer[4];      // 0xE000E180
+  volatile uint32_t rsvd7[28];    // 0xE000E190
+  volatile uint32_t ipsr[4];      // 0xE000E200
+  volatile uint32_t rsvd8[28];    // 0xE000E210
+  volatile uint32_t iprr[4];      // 0xE000E280
+  volatile uint32_t rsvd9[28];    // 0xE000E290
+  volatile uint32_t iactr[4];     // 0xE000E300
+  volatile uint32_t rsvd10[60];   // 0xE000E310
+  volatile uint32_t iprior[64];   // 0xE000E400
+  volatile uint32_t rsvd11[516];  // 0xE000E500
+  volatile uint32_t sctlr;        // 0xE000ED10
 };
 extern struct PFICRegMap pfic;
 
@@ -141,15 +141,15 @@ enum PFICIntPriority : uint8_t {
 
 #if defined(LIBCH32_V203_FAMILY) || defined(LIBCH32_V307_FAMILY)
 enum PFICIntNum : uint8_t {
-  PFIC_DMA1_CH1_INT_NUM = 27, // Addr: 0x0000006C
-  PFIC_DMA1_CH4_INT_NUM = 30, // Addr: 0x00000078
+  PFIC_DMA1_CH1_INT_NUM = 27,  // Addr: 0x0000006C
+  PFIC_DMA1_CH4_INT_NUM = 30,  // Addr: 0x00000078
   PFIC_CAN1_RX0_INT_NUM = 36,
   PFIC_CAN1_RX1_INT_NUM = 37,
-  PFIC_USART1_INT_NUM = 53, // Addr: 0x000000D4
+  PFIC_USART1_INT_NUM = 53,  // Addr: 0x000000D4
 };
 #elif defined(LIBCH32_V003_FAMILY)
 enum PFICIntNum {
-  PFIC_DMA1_CH4_INT_NUM = 25, // Addr: 0x00000064
+  PFIC_DMA1_CH4_INT_NUM = 25,  // Addr: 0x00000064
 };
 #else
 #error "unsupported device"
@@ -255,5 +255,4 @@ void core_init(void);
  */
 void core_pfic_set_int_priority(enum PFICIntNum in, enum PFICIntPriority prio);
 
-enum PFICIntPriority
-core_pfic_set_int_priority_threshold(enum PFICIntPriority prio);
+enum PFICIntPriority core_pfic_set_int_priority_threshold(enum PFICIntPriority prio);
--- lib/src/core.c
+++ lib/src/core.c
@@ -18,19 +18,19 @@ struct SysTickRegMap __attribute__((section(".systick"))) systick;
 
 struct PFICRegMap __attribute__((section(".pfic"))) pfic;
 
-static uint32_t _us_tick_count; // systicks per us
-static uint32_t _ms_tick_count; // systicks per ms
+static uint32_t _us_tick_count;  // systicks per us
+static uint32_t _ms_tick_count;  // systicks per ms
 
 #if LIBCH32_SYS_TICK_WIDTH == 32
 void core_delay_us(uint32_t duration) {
   (void)duration;
   if (_us_tick_count == 0) {
-    const struct RCCCfgValues *clk = get_clk_values();
-    _us_tick_count = clk->hclk_freq / 8000000; // 8... as STCLK=HCLK/8
+    const struct RCCCfgValues* clk = get_clk_values();
+    _us_tick_count = clk->hclk_freq / 8000000;  // 8... as STCLK=HCLK/8
     _ms_tick_count = _us_tick_count * 1000;
   }
 
-  systick.sr &= ~(STK_SR_CNTIF); // Clear compare flag
+  systick.sr &= ~(STK_SR_CNTIF);  // Clear compare flag
   systick.ctrl = 0;
   systick.cnt = 0;
   // systick.cmp = (uint32_t)duration * (uint32_t)_us_tick_count;
@@ -46,12 +46,12 @@ void core_delay_us(uint32_t duration) {
 #else
 void core_delay_us(uint32_t duration) {
   if (_us_tick_count == 0) {
-    _us_tick_count = rcc_get_clk_freq(RCC_CLOCK_ID_HCLK) / 8000000; // 8... as STCLK=HCLK/8
+    _us_tick_count = rcc_get_clk_freq(RCC_CLOCK_ID_HCLK) / 8'000'000;  // 8... as STCLK=HCLK/8
     _ms_tick_count = _us_tick_count * 1000;
   }
 
-  systick.sr &= ~(STK_SR_CNTIF); // Clear compare flag
-  systick.ctrl |= STK_CTLR_MODE; // Downcount
+  systick.sr &= ~(STK_SR_CNTIF);  // Clear compare flag
+  systick.ctrl |= STK_CTLR_MODE;  // Downcount
   systick.cmp = (uint64_t)duration * (uint64_t)_us_tick_count;
   systick.ctrl |= STK_CTLR_INIT | STK_CTLR_STE;
   while (1) {
@@ -67,8 +67,8 @@ void core_delay_ms(uint32_t duration) {
   core_delay_us(duration * 1000);
 }
 
-void *memset(void *ptr_in, int value, size_t count) {
-  uint8_t *ptr = (uint8_t *)ptr_in;
+void* memset(void* ptr_in, int value, size_t count) {
+  uint8_t* ptr = (uint8_t*)ptr_in;
   for (size_t i = 0; i < count; i++) {
     ptr[i] = (uint8_t)value;
   }
@@ -104,8 +104,8 @@ void core_pfic_set_int_priority(enum PFICIntNum in, enum PFICIntPriority prio) {
 #if defined(LIBCH32_CPU_CORE_V4B)
   union {
     struct {
-      uint8_t res :5; // 4:0
-      uint8_t pri :3; // 7:5
+      uint8_t res : 5;  // 4:0
+      uint8_t pri : 3;  // 7:5
     } sub[4];
     uint32_t dword;
   } reg;
@@ -118,14 +118,14 @@ void core_pfic_set_int_priority(enum PFICIntNum in, enum PFICIntPriority prio) {
 }
 
 void core_init(void) {
-
   // Initially set all PFIC interrupts to lower priority
   for (int idx = 0; idx < 256; idx++) {
     core_pfic_set_int_priority(idx, PFIC_INT_PRIORITY_LOWEST);
   }
 }
 
-enum PFICIntPriority core_pfic_set_int_priority_threshold(enum PFICIntPriority prio) {
+enum PFICIntPriority core_pfic_set_int_priority_threshold(
+    enum PFICIntPriority prio) {
   enum PFICIntPriority current_threshold = (pfic.ithresdr >> 5) & 0b111;
   pfic.ithresdr = (uint32_t)prio << 5;
   return current_threshold;
